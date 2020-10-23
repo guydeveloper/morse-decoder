123ff799f039a7de31b5ae1d25ee653547707a78
vetneka <44982805+vetneka@users.noreply.github.com>
2020-10-24 00:09:58 +0500

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const wordDivider = '**********';
+
+    const dict = {
+        '00': '',
+        '10': '.',
+        '11': '-'
+    };
+
+    const words = expr.split(wordDivider);
+    const chars = [];
+    const morseChars = [];
+
+    for (let i = 0; i < words.length; i += 1) {
+        const currentWord = words[i];
+        const currentChar = [];
+        for (let j = 1; j <= currentWord.length / 10; j += 1) {
+            let char = currentWord.substring(
+                currentWord.length - (10 * j),
+                currentWord.length - (10 * j) + 10);
+            if (char.length === 10) {
+                currentChar.push(char);
+            } else {
+                char = char.padStart(10, '0');
+                currentChar.push(char);
+            }
+        }
+        chars.push(currentChar);
+    }
+
+    for (let i = 0; i < chars.length; i += 1) {
+        const currentWord = chars[i];
+        const currentMorseWord = [];
+
+        for (let j = 0; j < currentWord.length; j += 1) {
+            const currentChar = currentWord[j];
+            const morseChar = [];
+
+            for (let k = 0; k < currentChar.length; k += 2) {
+                const currentMorseChar = currentChar.substring(k, k + 2);
+                morseChar.push(dict[currentMorseChar]);
+            }
+
+            currentMorseWord.push(morseChar.join(''));
+        }
+        morseChars.push(currentMorseWord);
+    }
+
+    const decode = morseChars.map((char) => {
+        return char
+            .map((item) => MORSE_TABLE[item])
+            .reverse()
+            .join('');
+    }).join(' ');
+
+    return decode;
 }
 
 module.exports = {
